5400a93f7ec443b74a5441b7980362d03c69872e
Martin <martin.mphil@gmail.com>
2025-04-09 08:51:47 +0100

    plain HTML instructions for candidates

---
+++ app/Instruct.tsx
@@ -0,0 +1,16 @@
+export default function Instruct() {
+  return (
+    <section className="instruct">
+      <h2>Instructions</h2>
+      <p>
+        Please select one of the buttons on the left to pick your <strong>best</strong> (most effective) response, AND select one of the buttons on the right to pick your <strong>worst</strong> (least effective) response.
+      </p>
+      <p>
+        Pressing the same Best or Worst button a second time deselects your answer and resets your choices.
+      </p>
+      <p>
+        After selecting your answers, press the <strong>submit</strong> button to submit your <em>final</em> answer.
+      </p>
+    </section>
+  )
+}
